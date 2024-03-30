61d164ee910de239dcd992fc3344cf4dfe806c48
Linh <dangdiemlinh04@gmail.com>
2024-03-30 12:25:00 -0400

    init commit

---
+++ futon-v1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  futon-v1
+//
+//  Created by Linh Dang on 3/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ futon-v1/futon_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  futon_v1App.swift
+//  futon-v1
+//
+//  Created by Linh Dang on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct futon_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
